e9268859633627c8e5cb95d80947d5cfacda8203
anhduc291203 <phamanhduc.ls2003@gmail.com>
2023-10-24 14:47:32 +0700

    init commit

---
+++ src/main/java/com/example/kafkaconnectorplugins/KafkaConnectorPluginsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaconnectorplugins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConnectorPluginsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConnectorPluginsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaconnectorplugins/KafkaConnectorPluginsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaconnectorplugins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConnectorPluginsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
